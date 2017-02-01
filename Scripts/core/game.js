58834c507ee04fec4bc8ba859f41d041d407c93c
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-02-01 10:43:29 -0500

    added Button Class

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -7,21 +7,30 @@
     var stage;
     // Game Variables
     var helloLabel;
+    var clickMeButton;
     function Start() {
         canvas = document.getElementById("canvas"); // get ref to canvas element
         stage = new createjs.Stage(canvas); // creating a new Stage Object
+        stage.enableMouseOver(20); // enable mouse over events every 20 frames
         createjs.Ticker.framerate = 60; // set fps to 60
         createjs.Ticker.on("tick", Update); // calls the Update function every framerate
         Game();
     }
     function Update(event) {
-        helloLabel.rotation += 5; // rotate clockwise 5 degrees every frame
         stage.update(); // manually redraws the stage
     }
     function Game() {
         console.log("Game Started...");
+        // add helloLabel to the stage
         helloLabel = new objects.Label("Hello, World!", "40px Consolas", "#000000", 160, 240, true);
         stage.addChild(helloLabel);
+        // add clickMe Button to the stage
+        clickMeButton = new objects.Button("../../Assets/images/ClickMeButton.png", 160, 300, true);
+        stage.addChild(clickMeButton);
+        // register an event listener with an anonymous event handler inline
+        clickMeButton.on("click", function (event) {
+            helloLabel.text = "Good Bye!";
+        });
     }
     window.onload = Start; // calls the Start function when the window finishes loading
 })();
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -11,10 +11,12 @@ let stage:createjs.Stage;
 
 // Game Variables
 let helloLabel:objects.Label;
+let clickMeButton:objects.Button;
 
 function Start():void {
   canvas = document.getElementById("canvas"); // get ref to canvas element
   stage = new createjs.Stage(canvas); // creating a new Stage Object
+  stage.enableMouseOver(20); // enable mouse over events every 20 frames
   createjs.Ticker.framerate = 60; // set fps to 60
   createjs.Ticker.on("tick", Update); // calls the Update function every framerate
 
@@ -23,16 +25,25 @@ function Start():void {
 
 function Update(event:createjs.Event):void {
 
-    helloLabel.rotation += 5; // rotate clockwise 5 degrees every frame
-
     stage.update(); // manually redraws the stage
 }
 
 function Game():void {
   console.log("Game Started...");
 
+  // add helloLabel to the stage
   helloLabel = new objects.Label("Hello, World!", "40px Consolas", "#000000", 160, 240, true);
   stage.addChild(helloLabel);
+
+  // add clickMe Button to the stage
+  clickMeButton = new objects.Button("../../Assets/images/ClickMeButton.png", 160, 300, true);
+  stage.addChild(clickMeButton);
+
+  // register an event listener with an anonymous event handler inline
+  clickMeButton.on("click", (event:createjs.MouseEvent) => {
+    helloLabel.text = "Good Bye!";
+  });
+
 }
 
 window.onload = Start; // calls the Start function when the window finishes loading
