5bad0c3011021e186c4e38abf73454676a123b97
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-02-01 09:35:43 -0500

    created Hello World Text Object

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -3,8 +3,40 @@
 // IIFE - Immediately Invoked Function Expression
 
 (function(){
-console.log("Game Started");
+// CreateJS Variables
+let canvas:HTMLElement;
+let stage:createjs.Stage;
 
+// Game Variables
+let helloLabel:createjs.Text;
 
+function Start():void {
+  canvas = document.getElementById("canvas"); // get ref to canvas element
+  stage = new createjs.Stage(canvas); // creating a new Stage Object
+  createjs.Ticker.framerate = 60; // set fps to 60
+  createjs.Ticker.on("tick", Update); // calls the Update function every framerate
+
+  Game();
+}
+
+function Update(event:createjs.Event):void {
+
+    helloLabel.rotation += 5; // rotate clockwise 5 degrees every frame
+
+    stage.update(); // manually redraws the stage
+}
+
+function Game():void {
+  console.log("Game Started...");
+
+  helloLabel = new createjs.Text("Hello, World!", "40px Consolas", "#000000");
+  helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+  helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+  helloLabel.x = 160;
+  helloLabel.y = 240;
+  stage.addChild(helloLabel);
+}
+
+window.onload = Start; // calls the Start function when the window finishes loading
 
 })();
